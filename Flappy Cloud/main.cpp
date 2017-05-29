b95d2ac7548b6d255f5fb43f6623053ae8172fd5
Nicopatsch <nicolas.patschkowski@telecom-sudparis.eu>
2017-05-29 18:52:20 +0200

    Kill, obstacles...

--- Flappy Cloud/main.cpp
+++ Flappy Cloud/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <Box2D/Box2D.h>
+#include <vector>
 
 
 // Here is a small helper for you! Have a look.
@@ -19,8 +20,143 @@ void loadVariables();
 
 static float gravityX;
 static float gravityY;
-static float velocityY = -5;
+static float velocityY;
 static float velocityX;
+
+enum obstacleType {storm, tornado};
+
+class Cloud {
+private:
+    b2BodyDef bodyDef;
+    b2Body* body;
+    b2PolygonShape shape;
+    b2FixtureDef fixtureDef;
+    sf::Texture cloudTexture;
+    sf::Sprite sprite;
+public:
+    void jump() {
+        body->SetLinearVelocity(b2Vec2(body->GetLinearVelocity().x,velocityY));
+    }
+    
+    Cloud(b2World& world)
+    {
+        cloudTexture.loadFromFile(resourcePath() + "cloud.png");
+        bodyDef.position = b2Vec2(10, 10);
+        bodyDef.type = b2_dynamicBody;
+        body = world.CreateBody(&bodyDef);
+        shape.SetAsBox((100.f/2)/SCALE, (67.f/2)/SCALE);
+        fixtureDef.density = 1.f;
+        fixtureDef.friction = 0.f;
+        fixtureDef.restitution = 0.1f;
+        fixtureDef.shape = &shape;
+        body->CreateFixture(&fixtureDef);
+        body->SetLinearVelocity(b2Vec2(velocityX, 0));
+        body->SetFixedRotation(true);
+    }
+    
+    void draw(sf::RenderWindow& window) {
+        sprite.setTexture(cloudTexture);
+        sprite.setOrigin(16.f, 16.f);
+        sprite.setPosition(SCALE * body->GetPosition().x, SCALE * body->GetPosition().y);
+        sprite.setRotation(body->GetAngle() * 180/b2_pi);
+        window.draw(sprite);
+    }
+    
+    float getPositionX() {
+        return body->GetPosition().x;
+    }
+    
+    void kill() {
+        body->SetTransform(b2Vec2(body->GetPosition().x, body->GetPosition().y), b2_pi);
+        body->SetLinearVelocity(b2Vec2(0,0));
+        body->SetType(b2_staticBody);
+        cout << "Cloud dead --> Game over!" <<endl;
+    }
+};
+
+
+class Ground {
+private:
+    b2BodyDef bodyDef;
+    b2Body* body;
+    b2PolygonShape shape;
+    b2FixtureDef fixtureDef;
+    sf::Texture groundTexture;
+    sf::Sprite sprite;
+public:
+    Ground(b2World& world, float X, float Y) {
+        bodyDef.position = b2Vec2(X/SCALE, Y/SCALE);
+        bodyDef.type = b2_staticBody;
+        body = world.CreateBody(&bodyDef);
+        shape.SetAsBox((800.f/2)/SCALE, (16.f/2)/SCALE);
+        fixtureDef.density = 0.f;
+        fixtureDef.shape = &shape;
+        body->CreateFixture(&fixtureDef);
+        groundTexture.loadFromFile(resourcePath() + "ground.png");
+    }
+    
+    void draw(sf::RenderWindow& window) {
+        sprite.setTexture(groundTexture);
+        sprite.setOrigin(400.f, 8.f);
+        sprite.setPosition(body->GetPosition().x * SCALE, body->GetPosition().y * SCALE);
+        sprite.setRotation(180/b2_pi * body->GetAngle());
+        window.draw(sprite);
+    }
+};
+
+
+class Obstacle {
+private:
+    b2BodyDef bodyDef;
+    b2Body* body;
+    b2PolygonShape shape;
+    b2FixtureDef fixtureDef;
+    sf::Texture texture;
+    sf::Sprite sprite;
+public:
+    Obstacle(b2World& world, float X, float Y, float width, float height, obstacleType type) {
+        bodyDef.position = b2Vec2(X/SCALE, Y/SCALE);
+        bodyDef.type = b2_staticBody;
+        body = world.CreateBody(&bodyDef);
+        shape.SetAsBox((width/2)/SCALE, (height/2)/SCALE);
+        fixtureDef.density = 0.f;
+        fixtureDef.shape = &shape;
+        body->CreateFixture(&fixtureDef);
+        if(type == obstacleType::storm) {
+            texture.loadFromFile(resourcePath() + "storm.png");
+            sprite.setOrigin(50.f, 50.f);
+        } else if(type == obstacleType::tornado) {
+            texture.loadFromFile(resourcePath() + "tornado.png");
+            sprite.setOrigin(50.f, 74.5f);
+        }
+    }
+    
+    void draw(sf::RenderWindow& window) {
+        sprite.setTexture(texture);
+        sprite.setPosition(body->GetPosition().x * SCALE, body->GetPosition().y * SCALE);
+        sprite.setRotation(180/b2_pi * body->GetAngle());
+        window.draw(sprite);
+        
+    }
+};
+
+
+class Storm: public Obstacle {
+private:
+    
+public:
+    Storm(b2World& world, float X, float Y): Obstacle(world, X, Y,  100.f, 100.f, obstacleType::storm) {
+    }
+};
+
+
+class Tornado: public Obstacle {
+private:
+    
+public:
+    Tornado(b2World& world, float X, float Y): Obstacle(world, X, Y,  100.f, 149.f, obstacleType::tornado) {
+    }
+};
 
 int main(int, char const**)
 {
@@ -29,18 +165,20 @@ int main(int, char const**)
     /** Prepare the world */
     b2Vec2 gravity(gravityX, gravityY);
     b2World world(gravity);
-    createGround(world, 400.f, 500.f);
-    b2Body* cloud = createCloud(world);
+    Ground ground = Ground(world, 400.f, 500.f);
+    Cloud cloud = Cloud(world);
+    vector<Obstacle> obstacles = vector<Obstacle>();
+    for (int i=0 ; i<100 ; i++) {
+        obstacles.push_back(Storm(world, i*1000, 100));
+        obstacles.push_back(Tornado(world, (i+0.5)*1000, 400));
+    }
     
-    sf::Texture groundTexture;
-    sf::Texture boxTexture;
-    sf::Texture cloudTexture;
-    groundTexture.loadFromFile(resourcePath() + "ground.png");
-    boxTexture.loadFromFile(resourcePath() + "box.png");
-    cloudTexture.loadFromFile(resourcePath() + "cloud.png");
+    
     
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::View view(sf::FloatRect(0, 0, 800, 600));
+    window.setView(view);
     
     
     // Clear screen
@@ -66,7 +204,11 @@ int main(int, char const**)
             }
             
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space) {
-                cloudJump(cloud);
+                cloud.jump();
+            }
+            
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::K) {
+                cloud.kill();
             }
             
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Return) {
@@ -87,31 +229,16 @@ int main(int, char const**)
         
         window.clear(sf::Color::White);
         int bodyCount = 0;
-        
-        
-        
-        for (b2Body* bodyIterator = world.GetBodyList(); bodyIterator != 0; bodyIterator = bodyIterator->GetNext())
-        {
-            if (bodyIterator->GetType() == b2_dynamicBody)
-            {
-                sf::Sprite sprite;
-                sprite.setTexture(cloudTexture);
-                sprite.setOrigin(16.f, 16.f);
-                sprite.setPosition(SCALE * bodyIterator->GetPosition().x, SCALE * bodyIterator->GetPosition().y);
-                sprite.setRotation(bodyIterator->GetAngle() * 180/b2_pi);
-                window.draw(sprite);
-            }
-            else
-            {
-                sf::Sprite groundSprite;
-                groundSprite.setTexture(groundTexture);
-                groundSprite.setOrigin(400.f, 8.f);
-                groundSprite.setPosition(bodyIterator->GetPosition().x * SCALE, bodyIterator->GetPosition().y * SCALE);
-                groundSprite.setRotation(180/b2_pi * bodyIterator->GetAngle());
-                window.draw(groundSprite);
-            }
+        cloud.draw(window);
+        ground.draw(window);
+        for (auto obs = obstacles.begin() ; obs<obstacles.end(); obs++) {
+            obs->draw(window);
         }
+        
+        
         // Update the window
+        view.setCenter(SCALE*cloud.getPositionX(), 300);
+        window.setView(view);
         window.display();
     }
     
@@ -119,46 +246,23 @@ int main(int, char const**)
 }
 
 
-void createGround(b2World& world, float X, float Y)
+void createObstacle(b2World& world, float X, float Y)
 {
     b2BodyDef bodyDef;
-    bodyDef.position = b2Vec2(X/30.f, Y/30.f);
+    bodyDef.position = b2Vec2(850, 100);
+    bodyDef.position = b2Vec2(X/SCALE, Y/SCALE);
     bodyDef.type = b2_staticBody;
     b2Body* body = world.CreateBody(&bodyDef);
     
     b2PolygonShape shape;
-    shape.SetAsBox((800.f/2)/SCALE, (16.f/2)/SCALE);
+    shape.SetAsBox((16.f/2)/SCALE, (16.f/2)/SCALE);
     b2FixtureDef fixtureDef;
     fixtureDef.density = 0.f;
     fixtureDef.shape = &shape;
     body->CreateFixture(&fixtureDef);
-    
-    
-}
-
-void cloudJump(b2Body* cloud) {
-    cloud->SetLinearVelocity(b2Vec2(0,velocityY));
 }
 
 
-b2Body* createCloud(b2World& world)
-{
-    b2BodyDef bodyDef;
-    bodyDef.position = b2Vec2(10, 10);
-    bodyDef.type = b2_dynamicBody;
-    b2Body* body = world.CreateBody(&bodyDef);
-    
-    b2PolygonShape shape;
-    shape.SetAsBox((100.f/2)/SCALE, (67.f/2)/SCALE);
-    //    shape.SetAsBox((80.f)/SCALE, (53.5f)/SCALE);
-    b2FixtureDef fixtureDef;
-    fixtureDef.density = 1.f;
-    fixtureDef.friction = 0.7f;
-    fixtureDef.restitution = 0.1f;
-    fixtureDef.shape = &shape;
-    body->CreateFixture(&fixtureDef);
-    return body;
-}
 
 void loadVariables() {
     pugi::xml_document doc;
