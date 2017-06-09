9e47fb0e5682ab336e091cfc4f2992d76917acd2
Olivier Freyssinet <olivier.freyssinet@gmail.com>
2017-06-09 23:03:32 +0200

    vraie spécialisation des classes dérivées de Obstacle (suppression de l'attribut obstacleType)

--- Flappy Cloud/main.cpp
+++ Flappy Cloud/main.cpp
@@ -20,6 +20,8 @@ static float scoreCoeff;
 static float timeStep, velocityIterations, positionIterations;
 static float blockLength = 800.f;
 
+static float stormVelocityY; // Storm objects vertical velocity (moving obstacles)
+
 enum obstacleType {storm, tornado};
 
 class Cloud {
@@ -39,7 +41,7 @@ public:
     Cloud(b2World& world)
     {
         cloudTexture.loadFromFile(resourcePath() + "cloud.png");
-        bodyDef.position = b2Vec2(10, 10);
+        bodyDef.position = b2Vec2(300/SCALE, 200/SCALE);
         bodyDef.type = b2_dynamicBody;
         body = world.CreateBody(&bodyDef);
         shape.SetAsBox((100.f/2)/SCALE, (67.f/2)/SCALE);
@@ -50,21 +52,21 @@ public:
         body->CreateFixture(&fixtureDef);
         body->SetLinearVelocity(b2Vec2(velocityX, 0));
         body->SetFixedRotation(true);
-        
+
         //Prepare the sfml score text
         if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
             return EXIT_FAILURE;
         }
         sfScore = sf::Text(scoreText, font, 50);
         sfScore.setFillColor(sf::Color::Black);
-        
+
         dead = false;
     }
-    
+
     void jump() {
         body->SetLinearVelocity(b2Vec2(velocityX,velocityY));
     }
-    
+
     void draw(sf::RenderWindow& window) {
         sprite.setTexture(cloudTexture);
         sprite.setOrigin(16.f, 16.f);
@@ -72,9 +74,13 @@ public:
         sprite.setRotation(body->GetAngle() * 180/b2_pi);
         window.draw(sprite);
     }
-    
+
     float getPositionX() {
         return body->GetPosition().x;
+    }
+
+    float getPositionY() {
+        return body->GetPosition().y;
     }
     
     void kill() {
@@ -84,11 +90,11 @@ public:
         body->SetType(b2_staticBody);
         cout << "Cloud dead --> Game over!" << endl;
     }
-    
+
     float getScore() {
         return round(body->GetPosition().x/scoreCoeff);
     }
-    
+
     void drawScore(sf::RenderWindow& window) {
         //Re-creating and updating the text in the sfml text object
         streamScoreText.str("Score: ");
@@ -98,7 +104,7 @@ public:
         sfScore.setPosition((body->GetPosition().x)*SCALE-100, 0);
         window.draw(sfScore);
     }
-    
+
     bool isDead() {
         return dead;
     }
@@ -126,15 +132,15 @@ public:
         groundTexture.loadFromFile(resourcePath() + "ground.png");
         nbGrounds++;
     }
-    
+
     Ground() {
         nbGrounds++;
     }
-    
+
     ~Ground() {
         nbGrounds--;
     }
-    
+
     void draw(sf::RenderWindow& window) {
         sprite.setTexture(groundTexture);
         sprite.setOrigin(blockLength/2, 8.f);
@@ -166,15 +172,15 @@ public:
         ceillingTexture.loadFromFile(resourcePath() + "ceilling.png");
         nbCeillings++;
     }
-    
+
     Ceilling() {
         nbCeillings++;
     }
-    
+
     ~Ceilling() {
         nbCeillings--;
     }
-    
+
     void draw(sf::RenderWindow& window) {
         sprite.setTexture(ceillingTexture);
         sprite.setOrigin(blockLength/2, -13.f);
@@ -188,64 +194,81 @@ public:
 
 static int nbObstacles = 0;
 class Obstacle {
-private:
+protected: // Protected: un objet de classe dérivée peut accéder à ses propres attributs définis dans la classe mère
     b2BodyDef bodyDef;
     b2Body* body;
     b2PolygonShape shape;
     b2FixtureDef fixtureDef;
     sf::Texture texture;
     sf::Sprite sprite;
-    obstacleType type;
+//    obstacleType type;
+    
 public:
-    Obstacle(b2World& world, float X, float Y, float width, float height, obstacleType type) {
-        this->type = type;
+    Obstacle(b2World& world, float X, float Y, float width, float height) {
+//        this->type = type;
         bodyDef.position = b2Vec2(X/SCALE, Y/SCALE);
-        bodyDef.type = b2_staticBody;
+        bodyDef.type = b2_kinematicBody;
         body = world.CreateBody(&bodyDef);
         shape.SetAsBox((width/2)/SCALE, (height/2)/SCALE);
         fixtureDef.density = 0.f;
         fixtureDef.shape = &shape;
         body->CreateFixture(&fixtureDef);
-        if(type == obstacleType::storm) {
-            texture.loadFromFile(resourcePath() + "storm.png");
-            sprite.setOrigin(50.f, 50.f);
-        } else if(type == obstacleType::tornado) {
-            texture.loadFromFile(resourcePath() + "tornado.png");
-            sprite.setOrigin(50.f, 74.5f);
-        }
+//        if(type == obstacleType::storm) {
+//            texture.loadFromFile(resourcePath() + "storm.png");
+//            sprite.setOrigin(50.f, 50.f);
+//        } else if(type == obstacleType::tornado) {
+//            texture.loadFromFile(resourcePath() + "tornado.png");
+//            sprite.setOrigin(50.f, 74.5f);
+//       }
         nbObstacles++;
     }
-    
+
     ~Obstacle() {
         nbObstacles--;
     }
 
-    
-    
+
+
     void draw(sf::RenderWindow& window) {
         sprite.setTexture(texture);
         sprite.setPosition(body->GetPosition().x * SCALE, body->GetPosition().y * SCALE);
         sprite.setRotation(180/b2_pi * body->GetAngle());
         window.draw(sprite);
-        
+
     }
 };
 
 
 class Storm: public Obstacle {
 private:
+    float topLimit;
+    float bottomLimit;
     
 public:
-    Storm(b2World& world, float X, float Y): Obstacle(world, X, Y,  100.f, 100.f, obstacleType::storm) {
+    Storm(b2World& world, float X, float Y): Obstacle(world, X, Y,  100.f, 100.f) {
+        topLimit = Y + 40;
+        bottomLimit = Y - 40;
+        
+        texture.loadFromFile(resourcePath() + "storm.png");
+        sprite.setOrigin(50.f, 50.f);
+        
+        body->SetLinearVelocity(b2Vec2(0, stormVelocityY));
+        topLimit = Y + 40;
+        bottomLimit =Y - 40;
     }
+    
+    
+    
 };
 
 
 class Tornado: public Obstacle {
 private:
-    
+
 public:
-    Tornado(b2World& world, float X, float Y): Obstacle(world, X, Y,  100.f, 149.f, obstacleType::tornado) {
+    Tornado(b2World& world, float X, float Y): Obstacle(world, X, Y,  100.f, 149.f) {
+        texture.loadFromFile(resourcePath() + "tornado.png");
+        sprite.setOrigin(50.f, 74.5f);
     }
 };
 
@@ -268,11 +291,11 @@ public:
         ceilling = Ceilling(world, (N+0.5)*blockLength);
         nbBlocks+=1;
     }
-    
+
     Block() {
-        
+
     }
-    
+
     ~Block() {
 //        ground.~Ground();
 //        for (auto it=obstacles.begin(); it<obstacles.end() ; it++) {
@@ -280,7 +303,7 @@ public:
 //        }
         nbBlocks--;
     }
-    
+
     void draw(sf::RenderWindow& window) {
         ground.draw(window);
         for (auto obs = obstacles.begin() ; obs<obstacles.end(); obs++) {
@@ -288,11 +311,11 @@ public:
         }
         ceilling.draw(window);
     }
-    
+
     float getPosition() {
         return (N+0.5)*blockLength;
     }
-    
+
     int getNbBlocks() {
         return nbBlocks;
     }
@@ -302,37 +325,37 @@ public:
 int main(int, char const**)
 {
     loadVariables();
-    
+
     /** Prepare the world */
     b2Vec2 gravity(gravityX, gravityY);
     b2World world(gravity);
     Cloud cloud(world);
-    
+
     int N=0;
-    
+
     vector<unique_ptr<Block>> blockPtrs;
     blockPtrs.push_back(make_unique<Block>(world, N-1));
     blockPtrs.push_back(make_unique<Block>(world, N));
     blockPtrs.push_back(make_unique<Block>(world, N+1));
-    
-    
+
+
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
     sf::View view(sf::FloatRect(0, 0, 800, 600));
     window.setFramerateLimit(1/timeStep);
     window.setView(view);
-    
-    
+
+
     // Clear screen
     window.clear();
-    
+
     sf::Clock clock;
     // Start the game loop
     while (window.isOpen())
     {
         sf::Time elapsed = clock.restart();
         world.Step(elapsed.asSeconds(), velocityIterations, positionIterations);
-        
+
         // Process events
         sf::Event event;
         while (window.pollEvent(event))
@@ -341,35 +364,36 @@ int main(int, char const**)
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-            
+
             // Escape pressed: exit
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
                 window.close();
             }
-            
+
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space) {
                 cloud.jump();
             }
-            
+
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::K) {
                 cloud.kill();
             }
-            
+
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Return) {
                 loadVariables();
                 b2Vec2 gravity(gravityX, gravityY);
                 world.SetGravity(gravity);
             }
         }
-        
-        
+
+
         window.clear(sf::Color(119, 185, 246));
         cloud.draw(window);
         cloud.drawScore(window);
         blockPtrs[0]->draw(window);
         blockPtrs[1]->draw(window);
             blockPtrs[2]->draw(window);
-        cout << "blockPtrs[1]->getPosition() = " << blockPtrs[1]->getPosition() << ", cloud.getPositionX()*SCALE = " << cloud.getPositionX()*SCALE << endl;
+        cout << "blockPtrs[1]->getPosition() = " << blockPtrs[1]->getPosition() << ", cloud.getPositionX()*SCALE = " << cloud.getPositionX()*SCALE<< ", cloud.getPositionY()*SCALE = " << cloud.getPositionY()*SCALE << endl;
+        
         cout << "nbBlocks = " << nbBlocks << ", nbGrounds = " << nbGrounds << ", nbObstacles = " << nbObstacles << endl;
         if(cloud.getPositionX()*SCALE>blockPtrs[1]->getPosition()) {
             cout << "Passage à droite" <<endl;
@@ -377,31 +401,33 @@ int main(int, char const**)
                 blockPtrs.erase(blockPtrs.begin());
             blockPtrs.push_back(make_unique<Block>(world, N));
             N+=1;
-            
+
             cout << "Block numéro " << N << " construit, " << N-2 << " détruit." << endl;
         }
-        
+
         // Update the window
         view.setCenter(SCALE*cloud.getPositionX(), 300);
         window.setView(view);
         window.display();
     }
-    
+
     return EXIT_SUCCESS;
 }
 
 
 void loadVariables() {
     pugi::xml_document doc;
-    
+
     if (!doc.load_file("../../../../../../../../FlappyCloud/Flappy Cloud/parameters.xml")) cout << "Failed loading file" << endl;
-    
+
     pugi::xml_node parameters = doc.child("Parameters");
-    
+
     gravityX=stof(parameters.child("Gravity").attribute("GravityX").value());
     gravityY=stof(parameters.child("Gravity").attribute("GravityY").value());
     velocityX=stof(parameters.child("Velocity").attribute("VelocityX").value());
     velocityY=stof(parameters.child("Velocity").attribute("VelocityY").value());
+    stormVelocityY=stof(parameters.child("StormVelocity").attribute("StormVelocityY").value());
+
     scoreCoeff=stof(parameters.child("Score").attribute("ScoreCoeff").value());
     timeStep=stof(parameters.child("Iterations").attribute("TimeStep").value());
     velocityIterations=stof(parameters.child("Iterations").attribute("VelocityIterations").value());
