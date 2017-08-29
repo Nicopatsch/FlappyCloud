740643ec556173d1ce5e2d64f078bb3b7cdc01f1
Nicopatsch <nicolas.patschkowski@telecom-sudparis.eu>
2017-08-29 14:21:22 -0400

    whatevs

---
+++ Flappy Cloud/rainbow.hpp
@@ -0,0 +1,14 @@
+//
+//  rainbow.hpp
+//  FlappyCloud
+//
+//  Created by Olivier Freyssinet on 29/08/2017.
+//  Copyright © 2017 Appdea. All rights reserved.
+//
+
+#ifndef rainbow_hpp
+#define rainbow_hpp
+
+#include <stdio.h>
+
+#endif /* rainbow_hpp */
